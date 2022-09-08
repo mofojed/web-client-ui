03bb0e625504a0ec90dd8c27d019acdc70ed081b
mikebender <mikebender@deephaven.io>
2022-09-08 09:36:28 -0400

    WIP start writing tests for console input
    It doesn't work, probably just need monaco to work in the tests

--- packages/console/src/Console.test.tsx
+++ packages/console/src/Console.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import dh from '@deephaven/jsapi-shim';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Console } from './Console';
 import { CommandHistoryStorage } from './command-history';
 
@@ -13,7 +14,13 @@ function makeMockCommandHistoryStorage(): CommandHistoryStorage {
   };
 }
 
-jest.mock('./ConsoleInput', () => () => null);
+jest.mock(
+  './ConsoleInput',
+  () =>
+    function MockConsoleInput({ onSubmit }) {
+      return <input onSubmit={event => onSubmit(event.target.value)} />;
+    }
+);
 jest.mock('./Console', () => ({
   ...(jest.requireActual('./Console') as Record<string, unknown>),
   commandHistory: jest.fn(),
@@ -37,4 +44,26 @@ function makeConsoleWrapper() {
 
 it('renders without crashing', () => {
   makeConsoleWrapper();
+});
+
+it('doesnt add blank items to history', async () => {
+  const { container } = makeConsoleWrapper();
+
+  // const consoleInput = container.querySelector('.console-input');
+  const consoleInput = screen.getByRole('textbox');
+
+  if (!consoleInput) {
+    throw new Error('console input is null');
+  }
+  // Monaco editor doesn't have a native input, so need to just click into it and type on the page
+  // https://github.com/microsoft/playwright/issues/14126
+
+  await userEvent.click(consoleInput);
+
+  await userEvent.type(consoleInput, 'print("Foo"){enter}');
+  await userEvent.type(consoleInput, '{enter}');
+  await userEvent.type(consoleInput, 'print("Bar"){enter}');
+
+  await userEvent.type(consoleInput, '{up}');
+  expect(consoleInput.textContent).toBe('print("Bar")');
 });
