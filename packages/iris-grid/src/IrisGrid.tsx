c1da66c26944d24d5c911981f8198aa78ee4fa66
mikebender <mikebender@deephaven.io>
2023-01-28 15:43:42 -0500

    feat!: Needed to change up this iris-grid stuff

--- packages/iris-grid/src/IrisGrid.tsx
+++ packages/iris-grid/src/IrisGrid.tsx
@@ -4419,7 +4419,7 @@ export class IrisGrid extends Component<IrisGridProps, IrisGridState> {
         <CSSTransition
           in={isMenuShown}
           timeout={ThemeExport.transitionMidMs}
-          classNames="slide-left"
+          classNames="slide-left dfddddd"
           onEntering={this.handleAnimationStart}
           onEntered={this.handleAnimationEnd}
           onExiting={this.handleAnimationStart}
