29f16715018f269f1fc0920f11143363b3041522
mikebender <mikebender@deephaven.io>
2023-02-09 18:42:34 -0500

    WIP use a jsapi-bootstrap to import the JS API
    - So jsapi-shim doesn't have reference to env variables, do it this way and message down when the API is loaded
    - Unit tests are broken - need to clean up how this is actually imported... maybe a separate package jsapi-bootstrap? That we can mock out then? And jsapi-shim imports it to initialize `globalThis.dh` ?

---
+++ packages/code-studio/src/jsapi-bootstrap.ts
@@ -0,0 +1,37 @@
+// Import the module and set it on the window so the shim still work for now
+const apiUrl = `${import.meta.env.VITE_CORE_API_URL}/${
+  import.meta.env.VITE_CORE_API_NAME
+}`;
+try {
+  const dhModule = await import(/* @vite-ignore */ apiUrl);
+  globalThis.dh = dhModule.default ?? dhModule;
+  globalThis.postMessage(
+    {
+      source: '@deephaven/code-studio',
+      event: '@deephaven/jsapi-shim/EventAPILoadSuccess',
+    },
+    window.origin
+  );
+} catch (e) {
+  document.getElementById('root')?.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="modal d-block">
+      <div class="modal-dialog modal-dialog-centered theme-bg-light">
+        <div class="modal-content">
+          <div class="modal-body">
+            <h5 class="modal-title">Error: Unable to load API</h5>
+            <p class="text-break">Ensure the server is running and you are able to reach ${apiUrl}, then refresh the page.</p>
+          </div>
+        </div>
+      </div>
+    </div>`
+  );
+  globalThis.postMessage(
+    {
+      source: '@deephaven/code-studio',
+      event: '@deephaven/jsapi-shim/EventAPILoadFailure',
+      payload: `${e}`,
+    },
+    window.origin
+  );
+}
--- packages/jsapi-shim/src/index.ts
+++ packages/jsapi-shim/src/index.ts
@@ -1,7 +1,37 @@
-import shim from './dh';
+// The Deephaven API script isn't packaged as a module (yet), and is just included in index.html, exported to the global namespace
+// This include file is simply a wrapper so that it behaves like a module, and can be mocked easily for unit tests
+import type dhType from './dh.types';
 
-export default shim;
-export { default as dh } from './dh';
+declare global {
+  // eslint-disable-next-line vars-on-top,no-var
+  var dh: dhType;
+}
+
+await new Promise((resolve, reject) => {
+  // If we already have the JS API set in the global context, just use that
+  if (globalThis.dh != null) {
+    resolve(globalThis.dh);
+    return;
+  }
+
+  function messageListener(event: MessageEvent) {
+    if (event.data?.event === '@deephaven/jsapi-shim/EventAPILoadSuccess') {
+      globalThis.removeEventListener('message', messageListener);
+      resolve(globalThis.dh);
+    }
+    if (event.data?.event === '@deephaven/jsapi-shim/EventAPILoadFailure') {
+      globalThis.removeEventListener('message', messageListener);
+      reject(event.data?.payload ?? new Error('Unknown import error'));
+    }
+  }
+
+  globalThis.addEventListener('message', messageListener);
+});
+
+export const { dh } = globalThis;
+
+export default dh;
+// export { default as dh } from './dh';
 export { default as PropTypes } from './PropTypes';
 export * from './dh.types';
 export type { default as dhType } from './dh.types';
