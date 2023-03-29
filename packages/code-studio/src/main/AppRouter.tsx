65e220950e6f2438b304ceb18bb4bd89f40e35b5
Mike Bender <mikebender@deephaven.io>
2023-03-29 16:05:11 -0400

    WIP try and set up relative linking
    - Updated react-router-dom
    - Try and put in new routes
    - Need to do some stuff to make AppMainContainer work, bit of a pain

--- packages/code-studio/src/main/AppRouter.tsx
+++ packages/code-studio/src/main/AppRouter.tsx
@@ -1,20 +1,14 @@
 import React, { ReactElement } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppInit from './AppInit';
 
 function AppRouter(): ReactElement {
   return (
     <Router basename={import.meta.env.BASE_URL}>
-      <Switch>
-        <Route exact path="/" component={AppInit} />
-        <Route path="/notebook/:notebookPath+" component={AppInit} />
-        <Redirect from="*" to="/" />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<AppInit />} />
+        <Route path="notebook/:notebookPath+" element={<AppInit />} />
+      </Routes>
     </Router>
   );
 }
