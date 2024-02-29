6c5e4295f335aa047ab47271cd5608355c1cb129
Mike Bender <mikebender@deephaven.io>
2024-02-29 09:31:06 -0500

    Get rid of React and some other crap

--- packages/code-studio/src/index.tsx
+++ packages/code-studio/src/index.tsx
@@ -1,8 +1,20 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import Plotly from '@deephaven/chart';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: 'scattergl',
+
+  // Drawing with mode=lines and type=scattergl does not work on M1 Macs
+  mode: 'lines',
+};
+
+const trace2 = {
+  x: [1, 2, 3, 4],
+  y: [16, 5, 11, 9],
+  type: 'scattergl',
+};
+
+const data: any = [trace1, trace2];
+
+Plotly.newPlot('root', data);
