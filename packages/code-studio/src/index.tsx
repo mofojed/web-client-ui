f12dae1273df22463d5889fad0534da9c24fbbb2
Mike Bender <mikebender@deephaven.io>
2024-02-29 09:22:03 -0500

    Get rid of lazy loading

--- packages/code-studio/src/index.tsx
+++ packages/code-studio/src/index.tsx
@@ -1,13 +1,8 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-
-// Lazy load components for code splitting and also to avoid importing the jsapi-shim before API is bootstrapped.
-// eslint-disable-next-line react-refresh/only-export-components
-const App = React.lazy(() => import('./App'));
+import App from './App';
 
 ReactDOM.render(
-  <Suspense fallback="Loading ...">
-    <App />
-  </Suspense>,
+    <App />,
   document.getElementById('root')
 );
