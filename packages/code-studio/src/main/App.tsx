e0f4dd9ab1eaafb88d8e3217104eb6588a73c0f0
mikebender <mikebender@deephaven.io>
2025-07-23 21:55:45 -0400

    Open apps at startup example
    - Just commented it out
    - You can open a popup fine and place it but only within the confines of the monitor your window is within, even though the coordinate system is based off both monitors
      - You can get the screen location with window.screen.availLeft

--- packages/code-studio/src/main/App.tsx
+++ packages/code-studio/src/main/App.tsx
@@ -1,8 +1,17 @@
-import React, { type ReactElement } from 'react';
+import React, { useEffect, type ReactElement } from 'react';
 import { ToastContainer } from '@deephaven/components';
 import AppMainContainer from './AppMainContainer';
 
 function App(): ReactElement {
+  // Just show it opening a portal at startup, as if user was reloading their dashboard session
+  // You do get asked for popup permissions, users should allow
+  // useEffect(() => {
+  //   window.open(
+  //     `http://localhost:4000/?popoutId=xxx`,
+  //     '_blank',
+  //     'left=2500,top=100,width=800,height=300,popup=true'
+  //   );
+  // }, []);
   return (
     <div className="app">
       <AppMainContainer />
